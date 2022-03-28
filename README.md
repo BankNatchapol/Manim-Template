afe2b5663cda8d2f7e4bc3021d67a173fa1ab8eb
BankNatchapol <natchapol.pat@gmail.com>
2022-03-28 14:24:24 +0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Manim-Template
+## Run command 
+High quality
+```
+manim -pqh test.py Introduction
+```
+Medium quality
+```
+manim -pqm test.py Introduction
+```
+Low quality
+```
+manim -pql test.py Introduction
+```
---
+++ test.py
@@ -0,0 +1,10 @@
+from manim import *
+
+
+class Introduction(Scene):
+    def construct(self):
+        name = Tex("Erwin")
+        box = SurroundingRectangle(name, buff=0.5)
+        self.play(Write(name))
+        self.play(Create(box))
+        self.wait(2.0)
