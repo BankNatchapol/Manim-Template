931129127de415a75c42a77f306797266fd2f48e
BankNatchapol <natchapol.pat@gmail.com>
2022-03-28 23:28:31 +0700

    add thai text

---
+++ basic/ThaiTex.py
@@ -0,0 +1,10 @@
+from manim import *
+
+
+class ThaiTex(Scene):
+    def construct(self):
+        name = Text("สวัสดีครับ")
+        box = SurroundingRectangle(name, buff=0.5)
+        self.play(Write(name))
+        self.play(Create(box))
+        self.wait(2.0)
