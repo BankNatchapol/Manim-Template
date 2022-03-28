024e7d68fcc87abd590e9116e6403da132359980
BankNatchapol <natchapol.pat@gmail.com>
2022-03-28 14:26:03 +0700

    ignore

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from manim import *
 
 class Introduction(Scene):
     def construct(self):
-        name = Tex("Erwin")
+        name = Tex("Bank")
         box = SurroundingRectangle(name, buff=0.5)
         self.play(Write(name))
         self.play(Create(box))
